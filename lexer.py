59b2b1c8f5b695ba252211941e81279396a4c5d3
WillRpi <ninja.spy3@gmail.com>
2015-09-30 16:01:08 -0400

    Removed garbage

--- lexer.py
+++ lexer.py
@@ -105,10 +105,3 @@ def lex(string):
             else:
                 token+=string[i]
     return tokens;
-def parse(tokens):
-    
-cmd=input()
-args=cmd.split()
-if(args[0]=="read"):
-    f = open(args[1], "r", encoding="utf-8")
-    lex(f.read())
