730d1b9f263f132896ef4928972ba7c2f7f7bbf9
WillRpi <ninja.spy3@gmail.com>
2015-09-30 16:15:01 -0400

    made it work (maybe)

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import lexer as l
-cmd=input()
-args=cmd.split()
-if(args[0]=="read"):
-    f = open(args[1], "r", encoding="utf-8")
-    l.lex(f.read())
+f = open(sys.argv[1], "r", encoding="utf-8")
+l.lex(f.read())
